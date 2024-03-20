bf0ea1dacc083d98e62b5920e0e20c04976b7366
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-03-20 16:43:20 +0300

    added Javadoc to Validator class

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,13 +3,31 @@ import hexlet.code.schemas.StringSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.MapSchema;
 
+/**
+ * Validator class provides methods for creating various schema validators.
+ * This class can be extended to add custom validation methods.
+ */
 public class Validator {
+    /**
+     * Returns a new StringSchema instance.
+     * @return StringSchema instance.
+     */
     public StringSchema string() {
         return new StringSchema();
     }
+
+    /**
+     * Returns a new NumberSchema instance.
+     * @return NumberSchema instance.
+     */
     public NumberSchema number() {
         return new NumberSchema();
     }
+    
+    /**
+     * Returns a new MapSchema instance.
+     * @return MapSchema instance.
+     */
     public MapSchema map() {
         return new MapSchema(); }
 }
